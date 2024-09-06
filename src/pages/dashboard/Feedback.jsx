0d1594dac2f8ff3e41ead14d9a3f6822842c9559
Emmanuel <findm4@gmail.com>
2024-09-06 08:03:39 +0100

    fixed side bar

--- src/pages/dashboard/Feedback.jsx
+++ src/pages/dashboard/Feedback.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-import { Table, Button, Input } from "antd";
+import React, { useState } from "react";
+import { Table, Button, Input, Dropdown, Menu } from "antd";
 import { Link } from "react-router-dom";
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 import dots from "../../assets/images/icons/dots.png";
-import user_2 from '../../assets/images/user_2.png';
-import user_3 from '../../assets/images/user_3.png';
-import user_1 from '../../assets/images/user_1.png';
+import user_2 from "../../assets/images/user_2.png";
+import user_3 from "../../assets/images/user_3.png";
+import user_1 from "../../assets/images/user_1.png";
 import edit from "../../assets/images/icons/edit_outline.png";
 import bin from "../../assets/images/icons/bin.png";
 import archive from "../../assets/images/icons/archive.png";
@@ -19,7 +18,7 @@ const fullDataSource = [
     user_name: "Rory Mcllroy",
     user_img: user_2,
     email: "rorymcllrory24@gmail.com",
-    time: "01:00pm"
+    time: "01:00pm",
   },
   {
     key: 1,
@@ -27,7 +26,7 @@ const fullDataSource = [
     user_name: "Manuel Ugate",
     user_img: user_3,
     email: "emanuelUgate@utd.org",
-    time: "Yesterday"
+    time: "Yesterday",
   },
   {
     key: 2,
@@ -35,7 +34,7 @@ const fullDataSource = [
     user_name: "Alxis Sanchez",
     user_img: user_1,
     email: "alexisanchez09@gmail.com",
-    time: "Tuesday"
+    time: "Tuesday",
   },
   {
     key: 3,
@@ -43,12 +42,66 @@ const fullDataSource = [
     user_name: "Micah Richards",
     user_img: user_2,
     email: "micrichards1995@gmail.com",
-    time: "Tuesday"
+    time: "Tuesday",
   },
 ];
 
+const handleMenuClick = (e, record) => {
+  if (e.key === "reply") {
+    console.log("Reply", record);
+    // Handle view logic here
+  } else if (e.key === "archive") {
+    console.log("Archive", record);
+    // Handle edit logic here
+  } else if (e.key === "delete") {
+    console.log("Delete", record);
+    // Handle delete logic here
+  }
+};
+
+const getMenu = (record) => (
+  <Menu onClick={(e) => handleMenuClick(e, record)}>
+    <Menu.Item
+      key="reply"
+      icon={
+        <img
+          src={edit}
+          alt="Reply"
+          style={{ width: "16px", marginRight: "8px" }}
+        />
+      }
+    >
+      Reply
+    </Menu.Item>
+    <Menu.Item
+      key="archive"
+      icon={
+        <img
+          src={archive}
+          alt="Archive"
+          style={{ width: "16px", marginRight: "8px" }}
+        />
+      }
+    >
+      Archive
+    </Menu.Item>
+    <Menu.Item
+      key="delete"
+      icon={
+        <img
+          src={bin}
+          alt="Delete"
+          style={{ width: "16px", marginRight: "8px" }}
+        />
+      }
+    >
+      Delete
+    </Menu.Item>
+  </Menu>
+);
+
 const Feedback = () => {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [selectedRowKeys, setSelectedRowKeys] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -56,12 +109,18 @@ const Feedback = () => {
     setSearchText(value);
   };
 
-  const filteredDataSource = fullDataSource.filter((item) =>
-    item.user_name.toLowerCase().includes(searchText.toLowerCase()) ||
-    item.email.toLowerCase().includes(searchText.toLowerCase())
+  const filteredDataSource = fullDataSource.filter(
+    (item) =>
+      item.user_name.toLowerCase().includes(searchText.toLowerCase()) ||
+      item.email.toLowerCase().includes(searchText.toLowerCase())
   );
 
   const columns = [
+    {
+      title: "SN",
+      render: (_, __, index) => index + 1,
+      width: 70,
+    },
     {
       title: "Feedbacks",
       dataIndex: "id",
@@ -69,11 +128,16 @@ const Feedback = () => {
     {
       title: "User Profile",
       render: (text, record) => (
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <img 
-            src={record.user_img} 
-            alt="User" 
-            style={{ width: 30, height: 30, borderRadius: '50%', marginRight: 8 }} 
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={record.user_img}
+            alt="User"
+            style={{
+              width: 30,
+              height: 30,
+              borderRadius: "50%",
+              marginRight: 8,
+            }}
           />
           <span>{record.user_name}</span>
         </div>
@@ -88,10 +152,86 @@ const Feedback = () => {
       title: "",
       dataIndex: "time",
     },
-  
+
     {
-      title: (
-        <div className="flex items-center justify-between w-full">
+      title: "",
+      key: "operations",
+      render: (_, record) => (
+        <Dropdown
+          menu={{
+            items: [
+              {
+                key: "reply",
+                label: (
+                  <span className="flex items-center">
+                    <img
+                      src={edit}
+                      alt="Reply"
+                      style={{
+                        width: "17px",
+                        height: "17px",
+                        marginRight: "8px",
+                      }}
+                    />
+                    Reply
+                  </span>
+                ),
+              },
+              {
+                key: "archive",
+                label: (
+                  <span className="flex items-center">
+                    <img
+                      src={archive}
+                      alt="Deactivate"
+                      style={{
+                        width: "17px",
+                        height: "17px",
+                        marginRight: "8px",
+                      }}
+                    />
+                    Archive
+                  </span>
+                ),
+              },
+              {
+                key: "delete",
+                label: (
+                  <span className="flex items-center">
+                    <img
+                      src={bin}
+                      alt="Delete"
+                      style={{
+                        width: "17px",
+                        height: "17px",
+                        marginRight: "8px",
+                      }}
+                    />
+                    Delete
+                  </span>
+                ),
+              },
+            ],
+            onClick: (e) => handleMenuClick(e, record),
+          }}
+          trigger={["click"]}
+        >
+          <Button>
+            <img
+              src={dots}
+              alt="Actions"
+              className="flex items-center justify-center w-1"
+            />
+          </Button>
+        </Dropdown>
+      ),
+    },
+  ];
+
+  return (
+    <div className="relative top-14">
+      <div className="bg-white rounded p-4">
+        <div className="flex items-center w-full mb-3 justify-end">
           <Input.Search
             className="w-44 mr-4"
             placeholder="Search"
@@ -102,81 +242,7 @@ const Feedback = () => {
             Archived
           </Button>
         </div>
-      ),
-      render: (text, record) => (
-        <span className="flex items-center justify-center">
-          <Menu as="div" className="relative inline-block text-left ml-2">
-            <div>
-              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50">
-                <img
-                  src={dots}
-                  style={{ width: 3, height: 15 }}
-                  className="cursor-pointer"
-                />
-              </MenuButton>
-            </div>
-            <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none">
-              <div className="py-1">
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to={`/user/${record.key}`}
-                      state={{ record }}
-                      className={`${
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                      } px-4 py-2 text-sm flex items-center`}
-                    >
-                      <img src={edit} alt="" className="w-4 h-4 mr-1" />
-                      Reply
-                    </Link>
-                  )}
-                </MenuItem>
-              </div>
-  
-              <div className='py-1'>
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="#"
-                      className={`${
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                      } px-4 py-2 text-sm flex items-center`}
-                    >
-                      <img src={archive} alt="" className="w-4 h-4 mr-1" />
-                      Archive
-                    </Link>
-                  )}
-                </MenuItem>
-              </div>
-  
-              <div className="py-1">
-                <MenuItem>
-                  {({ active }) => (
-                    <Link
-                      to="#"
-                      className={`${
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                      } px-4 py-2 text-sm flex items-center`}
-                    >
-                      <img src={bin} alt="" className="w-4 h-4 mr-1" />
-                      Delete
-                    </Link>
-                  )}
-                </MenuItem>
-              </div>
-            </MenuItems>
-          </Menu>
-        </span>
-      ),
-      colSpan: 5,
-    },
-  ];
-
-
-  return (
-    <div className="relative top-14">
-      <div className="bg-white rounded p-4">
-        <Table 
+        <Table
           columns={columns}
           dataSource={filteredDataSource}
           size="small"
