90ace1b68df8e7f8909578523d745e7efeaec219
Emmanuel <findm4@gmail.com>
2024-10-11 10:43:59 +0100

    feedback message fixed

--- src/components/feedback/UserFeedback.jsx
+++ src/components/feedback/UserFeedback.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { Button, Card, Input, message } from "antd";
+import { Button, Card, Input, message, Upload } from "antd";
 import axios from "axios";
 import { UploadOutlined } from "@ant-design/icons";
 import close from "../../assets/images/icons/close.png";
@@ -12,6 +12,7 @@ const UserFeedback = () => {
   const { baseUrl, accessToken, loggedInUser } = useContext(Context);
   const [loading, setLoading] = useState(false);
   const [reply, setReply] = useState("");
+  const [files, setFiles] = useState([]); // State to manage uploaded files
   const [replies, setReplies] = useState(record?.replies || []);
 
   useEffect(() => {
@@ -28,20 +29,16 @@ const UserFeedback = () => {
           },
         });
 
-        console.log("for testing", response.data.feedback.replies);
-
         const feedbackArray = response.data.feedback || [];
         const sourcedData = feedbackArray.find(
           (feedback) => feedback._id === record.key
         );
 
-        if (sourcedData) {
-          const replies = sourcedData.replies || [];
-          setReplies(replies);
+        console.log("user feedback", feedbackArray);
+        console.log("user", sourcedData.sender.avatar);
 
-          // Extract all sender._id values
-          const senderIds = replies.map((reply) => reply.sender._id);
-          console.log("Sender IDs:", senderIds);
+        if (sourcedData) {
+          setReplies(sourcedData.replies || []);
         } else {
           message.error("Feedback record not found.");
         }
@@ -56,6 +53,59 @@ const UserFeedback = () => {
     getFeedBack();
   }, [record, baseUrl, accessToken]);
 
+  // const feedbackReply = async () => {
+  //   if (!record || !record.key) {
+  //     message.error("Invalid feedback record. Please refresh and try again.");
+  //     return;
+  //   }
+
+  //   const replyUrl = `${baseUrl}/feedback/admin-reply`;
+  //   const formData = new FormData();
+  //   formData.append("feedbackId", record.key);
+  //   formData.append("message", reply);
+  //   formData.append("files", files);
+
+  //   // Append files to form data
+  //   files.forEach((file) => {
+  //     formData.append("files", file);
+  //   });
+
+  //   setLoading(true);
+
+  //   try {
+  //     const response = await axios.post(replyUrl, formData, {
+  //       headers: {
+  //         Authorization: `Bearer ${accessToken}`,
+  //         "Content-Type": "multipart/form-data",
+  //       },
+  //     });
+
+  //     console.log(response)
+
+  //     const newReply = response.data;
+  //     const len = newReply.feedback.replies.length;
+
+  //     const record = {
+  //       message: newReply.feedback.replies[len - 1].message,
+  //       createdAt: newReply.feedback.replies[len - 1].createdAt,
+  //       sender: {
+  //         _id: loggedInUser._id,
+  //         fullName: loggedInUser.fullName,
+  //       },
+  //     };
+
+  //     setReplies((prevReplies) => [...prevReplies, record]);
+  //     setReply("");
+  //     setFiles([]); // Clear files after sending
+  //   } catch (error) {
+  //     const errorMessage =
+  //       error.response?.data?.error || "Error sending reply. Please try again.";
+  //     message.error(errorMessage);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
+
   const feedbackReply = async () => {
     if (!record || !record.key) {
       message.error("Invalid feedback record. Please refresh and try again.");
@@ -63,33 +113,41 @@ const UserFeedback = () => {
     }
 
     const replyUrl = `${baseUrl}/feedback/admin-reply`;
-    const payload = {
-      feedbackId: record.key,
-      message: reply,
-      files: [],
-    };
+    const formData = new FormData();
+    formData.append("feedbackId", record.key);
+    formData.append("message", reply);
+
+    // Append each file individually to the formData
+    files.forEach((file) => {
+      formData.append("files", file.originFileObj); // Use originFileObj for files from Ant Design Upload
+    });
 
     setLoading(true);
 
     try {
-      const response = await axios.post(replyUrl, payload, {
+      const response = await axios.post(replyUrl, formData, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "multipart/form-data",
         },
       });
 
+      console.log(response);
       const newReply = response.data;
-      // console.log(newReply);
-
       const len = newReply.feedback.replies.length;
+
       const record = {
         message: newReply.feedback.replies[len - 1].message,
         createdAt: newReply.feedback.replies[len - 1].createdAt,
-        userId: newReply.feedback.replies[len - 1].userId,
+        sender: {
+          _id: loggedInUser._id,
+          fullName: loggedInUser.fullName,
+        },
       };
 
       setReplies((prevReplies) => [...prevReplies, record]);
       setReply("");
+      setFiles([]); // Clear files after sending
     } catch (error) {
       const errorMessage =
         error.response?.data?.error || "Error sending reply. Please try again.";
@@ -99,8 +157,9 @@ const UserFeedback = () => {
     }
   };
 
-  console.log("current user", record.senderId);
-  console.log("admin id", loggedInUser._id);
+  const handleFileChange = ({ fileList }) => {
+    setFiles(fileList); 
+  };
 
   return (
     <div className="relative top-14">
@@ -136,27 +195,37 @@ const UserFeedback = () => {
               </div>
             </Card>
           </div>
-          <div className="flex flex-col">
-            {replies.length > 0 ? (
-              replies.map((reply, index) => (
+
+          {replies.length > 0 ? (
+            replies.map((reply, index) => (
+              <div
+                key={index}
+                className={`p-2 ${
+                  reply.sender && reply.sender._id === loggedInUser._id
+                    ? "flex justify-end" 
+                    : "flex justify-start" 
+                }`}
+              >
                 <div
-                  key={index}
-                  className={`flex ${
-                    reply.sender._id === loggedInUser._id
-                      ? "justify-end"
+                  className={`flex items-center gap-4 ${
+                    reply.sender && reply.sender._id === loggedInUser._id
+                      ? "flex-row-reverse justify-end" 
                       : "justify-start"
-                  } mb-2`}
+                  }`}
                 >
-                  <Card
-                    className={`${
-                      reply.sender._id === loggedInUser._id
-                        ? "bg-blue-100"
-                        : "bg-gray-200"
-                    } shadow-lg rounded-tr-lg rounded-br-lg rounded-bl-none w-full max-w-xs sm:max-w-md md:max-w-lg`}
-                  >
-                    <p className="break-words">{reply.message}</p>
-                    <div className="flex justify-end">
-                      <span className="text-xs">
+                  <div className="w-10 h-10 rounded-full overflow-hidden">
+                    <img
+                      src={reply.sender.avatar}
+                      alt=""
+                      className="object-cover"
+                    />
+                  </div>
+                  <div>
+                    <Card className="bg-[#F5F5F5] shadow-lg rounded-tr-lg rounded-br-lg rounded-bl-none w-96 max-w-full">
+                      <p>{reply.message}</p>
+                    </Card>
+                    <div className="flex justify-end mt-2">
+                      <span className="text-xs text-gray-400">
                         {new Date(reply.createdAt).toLocaleTimeString("en-US", {
                           hour: "2-digit",
                           minute: "2-digit",
@@ -164,21 +233,17 @@ const UserFeedback = () => {
                         })}
                       </span>
                     </div>
-                  </Card>
+                  </div>
                 </div>
-              ))
-            ) : (
-              <div className="flex justify-end">
-                <Card className="bg-[#F5F5F5] shadow-lg rounded-tr-lg rounded-br-lg rounded-bl-none w-full max-w-xs sm:max-w-md md:max-w-lg flex items-center justify-center">
-                  {loading ? (
-                    <div className="loader">Loading replies...</div> // Add your loader component here
-                  ) : (
-                    ""
-                  )}
-                </Card>
               </div>
-            )}
-          </div>
+            ))
+          ) : (
+            <div className="p-2 flex justify-end">
+              <Card className="bg-[#F5F5F5] shadow-lg rounded-tr-lg rounded-br-lg rounded-bl-none w-96 max-w-full">
+                <p>No replies yet</p>
+              </Card>
+            </div>
+          )}
         </div>
 
         <div className="mt-10">
@@ -190,6 +255,7 @@ const UserFeedback = () => {
             addonAfter={
               <Button
                 type="primary"
+                className="bg-[#F1B31C] hover:!bg-[#F1B31C] hover:!text-black border-none text-black"
                 onClick={feedbackReply}
                 style={{ borderRadius: "0" }}
                 disabled={loading}
@@ -198,7 +264,17 @@ const UserFeedback = () => {
               </Button>
             }
             addonBefore={
-              <Button icon={<UploadOutlined />} style={{ borderRadius: "0" }} />
+              <Upload
+                multiple
+                beforeUpload={() => false} // Prevent automatic upload
+                onChange={handleFileChange} // Handle file selection
+                fileList={files} // Controlled file state
+              >
+                <Button
+                  icon={<UploadOutlined className="hover:!text-black" />}
+                  className="bg-[#F1B31C] hover:!bg-[#F1B31C] rounded-none border-none"
+                ></Button>
+              </Upload>
             }
             style={{ borderRadius: "5px" }}
           />
